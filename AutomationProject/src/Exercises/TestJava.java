7a8ca7fe87fc35e74fe4653a335de8d63d09717b
Edjel Maramara <edjel.maramara@viseo.com>
2022-10-05 19:45:53 +0800

    change of name

--- AutomationProject/src/Exercises/TestJava.java
+++ AutomationProject/src/Exercises/TestJava.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Sampol1 {
+public class TestJava {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
